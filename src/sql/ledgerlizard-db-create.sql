fe9176025da3670ccb8a1b8dd162df302a3fe8e0
Zach Cortese <zachary@cortesecrew.com>
2022-03-17 16:58:14 -0400

    started database definition for SQL Server

---
+++ src/sql/ledgerlizard-db-create.sql
@@ -0,0 +1,20 @@
+IF (NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'ledgerlizard'))
+BEGIN
+	CREATE DATABASE [ledgerlizard];
+END
+
+USE [ledgerlizard];
+
+IF (NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'identity')) 
+BEGIN
+	EXEC ('CREATE SCHEMA [identity];')
+END
+
+IF (NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'user')) 
+BEGIN
+	CREATE TABLE [ledgerlizard].[identity].[user] (
+		[user_id] INT IDENTITY (1,1) PRIMARY KEY
+		, [username] VARCHAR(50)
+		, [password] VARCHAR(500) -- We will salt and encrypt this eventually, for now do not store any valuable password here
+	)
+END
